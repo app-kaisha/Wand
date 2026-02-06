322762820b346a8f5978b829f87e78a451555c4e
app-kaisha <app.kaihatsusha@gmail.com>
2026-02-06 15:42:07 +0000

    Initial Commit

---
+++ Wand/WandApp.swift
@@ -0,0 +1,18 @@
+//
+//  WandApp.swift
+//  Wand
+//
+//  Created by app-kaihatsusha on 06/02/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct WandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
