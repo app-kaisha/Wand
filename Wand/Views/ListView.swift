171a4c039aeb9cd50a3c6f015a0aa7c0ce8812dc
app-kaisha <app.kaihatsusha@gmail.com>
2026-02-06 18:37:21 +0000

    Initial Asset loading and setup of App

---
+++ Wand/Views/ListView.swift
@@ -0,0 +1,23 @@
+//
+//  ListView.swift
+//  Wand
+//
+//  Created by app-kaihatsusha on 06/02/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+// https://hp-api.onrender.com/api/spells
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        VStack {
+            Text("Potter Spells")
+        }
+    }
+}
+
+#Preview {
+    ListView()
+}
